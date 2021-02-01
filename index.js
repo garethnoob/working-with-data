9b72d3192c502e34f92e41841329b4739669ad69
Gareth Davies <gaz.andrew@gmail.com>
2021-02-01 21:59:30 +0000

    add start of server code

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // This is the server
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('./public/index.html'));
